7d65c4db86d5a9169fdc40a30d98471349f5e172
Yura Panayotov <panayotov.yura@gmail.com>
2015-06-08 00:00:30 +0300

    add tests

--- src/Levi9/StudentsBundle/DataFixtures/ORM/StudentsLoader.php
+++ src/Levi9/StudentsBundle/DataFixtures/ORM/StudentsLoader.php
@@ -12,8 +12,12 @@ class StudentsLoader extends DataFixtureLoader
      */
     protected function getFixtures()
     {
+        $fileName = 'fixtures.yml';
+        if ($this->container->get('kernel')->getEnvironment() == 'test') {
+            $fileName = 'fixtures_test.yml';
+        }
         return  array(
-            __DIR__ . '/fixtures.yml',
+            __DIR__ . '/' . $fileName,
 
         );
     }
--- src/Levi9/StudentsBundle/Tests/Controller/StudentsControllerTest.php
+++ src/Levi9/StudentsBundle/Tests/Controller/StudentsControllerTest.php
@@ -3,7 +3,23 @@
 namespace Levi9\StudentsBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Request;
 
 class StudentsControllerTest extends WebTestCase
 {
+    public function testDetailsPage()
+    {
+        $client = static::createClient();
+
+        $client->request(Request::METHOD_GET, '/students/detail/yura_panayotov_8');
+
+        $response = $client->getResponse();
+
+        $this->assertTrue($response->isSuccessful());
+
+        $this->assertInstanceOf(
+            'DateTime',
+            $response->getExpires()
+        );
+    }
 }
